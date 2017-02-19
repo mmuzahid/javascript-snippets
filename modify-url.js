29286a8a48676b951acf50a7c07b442835b3a4a7
mmuzahid <muzahid.engr@gmail.com>
2017-02-19 22:47:06 +0600

    Modified to represent a URL as Object
    Signed-off-by: mmuzahid <muzahid.engr@gmail.com>

--- modify-url.js
+++ modify-url.js
@@ -5,22 +5,28 @@
 * @author      MD. MUZAHIDUL ISLAM
 * @contact     CV.MUZAHID@GMAIL.COM  
 */
-var scheme;
-var user;
-var password;
-var host;
-var port;
-var path;
-var query;
-var fragment; 
+
+var ModifiedUrl = (function () {
+return function(urlStr) {
+
+
+this.scheme = undefined;
+this.user = undefined;
+this.password = undefined;
+this.host = undefined;
+this.port = undefined;
+this.path = undefined;
+this.query = undefined;
+this.fragment = undefined; 
 
 /**
  *	Upadate QueryString by given key and value
  *
  */
-function updateQueryString(url, key, value) {
+this.updateQueryString = function updateQueryString(key, value) {
+	var url = this.url;
 	var arr =  url.split("#");
-	var url = arr[0];
+	url = arr[0];
 	var fragmentId = arr[1];
 	var updatedQS = "";
 	if (url.indexOf("?") == -1) {
@@ -33,7 +39,7 @@ function updateQueryString(url, key, value) {
 	if (typeof fragmentId !== 'undefined') {
 		url = url + "#" + fragmentId;
 	}
-	return url;
+	this.setUrl(url);
 }
 
 /**
@@ -65,43 +71,45 @@ function updateValueForKey(queryStrings, key, value) {
  *	Process a URL and parse all part of it
  *
  */
-function processURL(url) {
+this.processURL = function processURL(url) {
 	//fragment
 	var partArray = url.split("#");
-	fragment = partArray[1];
+	this.fragment = partArray[1];
 	part = partArray[0];			
 	//query
 	partArray = part.split("?");
-	query = partArray[1];
+	this.query = partArray[1];
 	part = partArray[0];
 	//scheme
-	scheme = part.substring(0, part.indexOf(":"));
+	this.scheme = part.substring(0, part.indexOf(":"));
 	part = part.substring(part.indexOf(":") + 1);
 	if (part.startsWith("//")) {
 		part = part.substring(2);
 	}
 	//path
 	if (part.indexOf("/") != -1) {
-		path = part.substring(part.indexOf("/"));
+		this.path = part.substring(part.indexOf("/"));
 		part = part.substring(0, part.indexOf("/"));
 	}
 	//user & password
 	if (part.indexOf("@") != -1) {
 		partArray = part.split("@");
-		user = partArray[0].split(":")[0];
-		password = partArray[0].split(":")[1];
+		this.user = partArray[0].split(":")[0];
+		this.password = partArray[0].split(":")[1];
 		part = partArray[1];
 	}
 	//host and port
-	host = part.split(":")[0];
-	port = part.split(":")[1];
-	
-	/* console.log("scheme: " + scheme);
-	console.log("user: " + user);
-	console.log("password: " + password);
-	console.log("host: " + host);
-	console.log("port: " + port);
-	console.log("path: " + path);
-	console.log("query: " + query);
-	console.log("fragment: " + fragment); */
+	this.host = part.split(":")[0];
+	this.port = part.split(":")[1];
 }
+
+
+this.setUrl = function setUrl(url) {
+	this.url = url;
+	this.processURL(url);
+};
+
+this.setUrl(urlStr);
+
+
+}}) ();

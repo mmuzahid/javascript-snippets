2ec36ad3ff45a9cde2d03103c7ef4f441e35d34c
Md. Muzahidul Islam <muzahid.engr@gmail.com>
2020-06-18 00:10:33 +0600

    Code Refactored

--- jquery-dialog-hack-iframewrapper.js
+++ jquery-dialog-hack-iframewrapper.js
@@ -1,20 +1,17 @@
 /**
 * This is a jquery-dialog hack to show html content inside an iframe
 */
+
+let modalProperties = {autoOpen: true, width: 900, height: 600, modal: true, title: 'Modal Title'};
 let modalHtmlContent = '<div>My Content First div</div><div>My Content Second div</div>';
+
+// create wrapper iframe
 let wrapperIframe = $('<iframe src="" frameborder="0" style="width: 100%; height: 100%;"></iframe>');
 
-// create jquery dialog with iframe
-$("<div></div>").append(wrapperIframe).dialog({
-  autoOpen: false,
-  width: 900,
-  height: 600,
-  autoOpen: true,
-  modal: true,
-  title: 'My Modal Title'
-});
+// create jquery dialog by a 'div' with 'iframe' appended
+$("<div></div>").append(wrapperIframe).dialog(modalProperties);
 
-// insert html content to iframe body
+// insert html content to iframe 'body'
 let wrapperIframeDocument = wrapperIframe[0].contentDocument;
 let wrapperIframeBody = $('body', wrapperIframeDocument);
 wrapperIframeBody.html(modalHtmlContent);
